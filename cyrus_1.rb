fa0cb985e33ec8c4cb9e2b893009b0882cf14dd3
skellz <sherwin.kelly@gmail.com>
2014-02-04 01:17:14 -0500

    updated parse

--- cyrus_1.rb
+++ cyrus_1.rb
@@ -2,7 +2,6 @@ require 'date'
 require 'pry'
 
 class CyrusReader
-  attr_reader :filename
 
   def initialize(filename)
     @raw_data = File.open(filename).read
@@ -18,7 +17,7 @@ class CyrusReader
 
   def parse_pipe_file
     @raw_data.lines.collect do |line|
-      line = line.split(" | ").collect { |x| x.strip }
+      line = line.split("|").collect { |x| x.strip }
       @data[:LastName] << line[0]
       @data[:FirstName] << line[1]
       @data[:Gender] << set_gender(line[3])
@@ -29,24 +28,25 @@ class CyrusReader
   end
 
   def parse_comma_file
-    File.open(@filename) do |file|
-      file.each do |line|
-        info = line.split(", ")
-        data[:LastName] << info[0]
-        data[:FirstName] << info[1]
-        data[:Gender] << info[2]                  
-        data[:FavoriteColor] << info[3]
-        data[:DateOfBirth] << info[4].tr("\n", '')
+    @raw_data.lines.collect do |line|
+      line = line.split(",").collect { |x| x.strip }
+      @data[:LastName] << line[0]
+      @data[:FirstName] << line[1]
+      @data[:Gender] << info[2]                  
+      @data[:FavoriteColor] << info[3]
+      @data[:DateOfBirth] << info[4].tr("\n", '')
       end
     end
   end
 
   def parse_space_file
-    data[:LastName] << info[0]
-    data[:FirstName] << info[1]
-    data[:Gender] << set_gender(info[3])
-    data[:DateOfBirth] << set_date(info[4].tr("\n", ''))
-    data[:FavoriteColor] << info[5]
+    @raw_data.lines.collect do |line|
+      line = line.split(" ").collect { |x| x.strip }
+      @data[:LastName] << line[0]
+      @data[:FirstName] << line[1]
+      @data[:Gender] << set_gender(info[3])
+      @data[:DateOfBirth] << set_date(info[4])
+      @data[:FavoriteColor] << info[5]
   end
 
   def set_gender(gender)
